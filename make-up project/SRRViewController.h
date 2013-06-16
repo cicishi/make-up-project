1e26852b8628ff053a85ae8217da744f83f2e5c0
owen <owen@owentekiMacBook-Air.local>
2013-06-16 14:35:46 +0800

    Initial Commit

---
+++ make-up project/SRRViewController.h
@@ -0,0 +1,13 @@
+//
+//  SRRViewController.h
+//  make-up project
+//
+//  Created by owen on 13-6-16.
+//  Copyright (c) 2013年 owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SRRViewController : UIViewController
+
+@end
---
+++ make-up project/SRRViewController.m
@@ -0,0 +1,29 @@
+//
+//  SRRViewController.m
+//  make-up project
+//
+//  Created by owen on 13-6-16.
+//  Copyright (c) 2013年 owen. All rights reserved.
+//
+
+#import "SRRViewController.h"
+
+@interface SRRViewController ()
+
+@end
+
+@implementation SRRViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
